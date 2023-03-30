25634e5a789075172476f38352968ce41dda1a0b
Anifowose Morolake <morolaanney@gmail.com>
2023-03-30 12:08:02 +0100

    Final changes to the Python code

--- main.py
+++ main.py
@@ -5,22 +5,29 @@ from flask.globals import request
 
 from functions import Bank
 
+# app initialisation
 app = Flask(__name__)
+
 users = {}
+# to print the users to the console
 print(users)
-app.secret_key = 'hkjkjhft7yqr7526%$^"£"'
+app.secret_key = 'hhft7yqr7526%$^"£"'
 
 
+
+# defining the home page
 @app.route('/')
 def home():
   if 'user' in session:
-    
+    firstname = request.form.get('firstname')
+    lastname = request.form.get('lastname')
     money = user.viewBalance()
     return render_template('home.html', money=money)
     
   else:
     return render_template('home.html')
 
+# defining the login page
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
   global user
@@ -28,13 +35,11 @@ def login():
     username = request.form['username']
     password = request.form['password']
 
-    
-    #new addition
-    
 
     if username in users and users[username] == password:
       session['user'] = username
-      
+      session['fname'] = request.form['firstname']
+      session['lname'] = request.form['lastname']
       
 
       user = Bank(session['user'])
@@ -44,21 +49,18 @@ def login():
       print('Login Successful')
       return render_template('home.html',firstname = request.form.get('firstname'), lastname = request.form.get('lastname'))
     else:
-      print('Email dosent exist or wrong password')
+      print('Email doesnt exist or wrong password')
       return render_template('login.html')
       
 
   return render_template('login.html')
 
+
+# defining the register page
 @app.route('/register', methods= ['GET', 'POST'])
 def register():
-  global user
   if request.method == 'POST':
     username = request.form['username']
-    
-    # firstname = request.form['firstname']
-    # lastname = request.form['lastname']
-
     if username in users:
       print('User already exists')
       return render_template('register.html')
@@ -67,24 +69,37 @@ def register():
       users[username] = password
       print('user created')
       print(users)
+      
+      session['user'] = username
+      session['fname'] = request.form['firstname']
+      session['lname'] = request.form['lastname']
+      
       return render_template('home.html',firstname = request.form.get('firstname'), lastname = request.form.get('lastname'))
   else:
     return render_template('register.html')
 
-
+# defining the deposit page
 @app.route('/deposit', methods = ['GET', 'POST'])
 def deposit():
-  if request.method == 'POST':
+  global user
+  if request.method == 'POST' and 'user' in session:
+    user = Bank(session['user'])
     amount = request.form['deposit']
     user.deposit(amount)
     print(user.showDetails())
+    
+  
+    
     return render_template('deposit.html')
   else:
     return render_template('deposit.html')
 
+#defining the withdraw page
 @app.route('/withdraw', methods = ['GET', 'POST'])
 def withdraw():
+  global user
   if request.method == 'POST':
+    user = Bank(session['user'])
     amount = request.form['withdraw']
     user.withdraw(amount)
     print(user.showDetails())
@@ -92,9 +107,12 @@ def withdraw():
   else:
     return render_template('withdraw.html')
 
+# defining the transfer page
 @app.route('/transfer', methods = ['GET', 'POST'])
 def transfer():
+  global user
   if request.method == 'POST':
+    user = Bank(session['user'])
     amount = request.form['transfer']
     user.transfer(amount)
     print(user.showDetails())
@@ -102,10 +120,22 @@ def transfer():
   else:
     return render_template('transfer.html')
 
-@app.route('/logout')
-def logout():
-  session.pop('user', None)
-  return redirect(url_for('home'))
+# defining the dashboard page
+@app.route('/dashboard', methods = ['GET', 'POST'])
+def dashboard():
+  global user
+  firstname = request.form.get('firstname')
+  lastname = request.form.get('lastname')
+  if request.method == 'POST':
+    user = Bank(session['user'])
+    return render_template('home.html',firstname = firstname, lastname = lastname)
+  else:
+    return render_template('home.html',firstname = firstname, lastname = lastname)
+
+# @app.route('/logout')
+# def logout():
+#   session.pop('user', None)
+#   return redirect(url_for('home'))
   
 
 
